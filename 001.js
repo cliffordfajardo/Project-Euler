ab56a6d3ffbce923f8a62d5cfc0311164b6d8812
Clifford Fajardo <fajardoclifford@gmail.com>
2015-01-08 17:26:21 -0800

    updated code format

--- 001.js
+++ 001.js
@@ -31,7 +31,8 @@ multiple3or5_to(1000); //233168
 1. First, I started off by naming my function & parameter something that explains what I'm trying to do.
 
 2. Since we want to find out the total of a number, at the end, it makes to declare a variable, total, which we will
-   use to keep adding the multiples of 3 or 5, and make the inital value 0. I tried to leave the initial value of total undefined like this (var total;) , but undefined + a number equals NaN, so we need 0 to start.
+   use to keep adding the multiples of 3 or 5, and make the inital value 0. I tried to leave the initial value of total
+    undefined like this (var total;) , but undefined + a number equals NaN, so we need 0 to start.
 
 3. Create a for-loop with the condition that states, "while my condition (1000--) is true, I'll keep looping".
    Once my loop hits 0 the while loop no longer executes.
@@ -42,7 +43,9 @@ multiple3or5_to(1000); //233168
 **************************************** WHAT I LEARNED ***************************************
 1.  A new use of the decrement operator with a while loop:
     I knew already that a while loop only runs when the condition of it is true. But I totally forgot that the value
-    0 has a very special meaning in javascript, it can mean false also. So when my number is decremented all the way to down 0 it tells the while loop, "the condition is no longer true". Initially, I thought my (num--) would just decrement down infintely and cause an infinete loop producing negative numbers, but that's not the case.
+    0 has a very special meaning in javascript, it can mean false also. So when my number is decremented all the way to
+    down 0 it tells the while loop, "the condition is no longer true". Initially, I thought my (num--) would just
+    decrement down infintely and cause an infinete loop producing negative numbers, but that's not the case.
 
     If I input 1000 as my parameter, my num is immediately reduced to 999, then goes through the loop. That is to say
     the decrement starts immediately. I had thought it started from 1000, but that's not the case. (Test w/for)
